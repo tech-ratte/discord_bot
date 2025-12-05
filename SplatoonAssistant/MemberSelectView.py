27ee798907213b8366ff008b11b143fd1b8a8345
ratte2d3d <kinoyama001@gmail.com>
2025-12-05 14:40:36 +0900

    修了時・タイムアウト（None）を実装

--- SplatoonAssistant/MemberSelectView.py
+++ SplatoonAssistant/MemberSelectView.py
@@ -6,10 +6,9 @@ from TeamControlView import TeamControlView
 # ユーザー選択メニューを定義する View クラス
 class MemberSelectView(View):
 
-
     # タイムアウト時間など、Viewの基本設定をここに書く
     def __init__(self, start_time, count=1, record=[]):
-        super().__init__(timeout=180)
+        super().__init__(timeout=None)
 
         # 日付と試合回数と戦績
         self.start_time = start_time
@@ -19,21 +18,20 @@ class MemberSelectView(View):
         self.init_embed = discord.Embed(
             title="👥 メンバー選択",
             description="▼ 参加メンバーを以下から選択してください",
-            color=discord.Color.blurple()
+            color=discord.Color.blurple(),
         )
-
 
     # ユーザー選択メニュー（UserSelect）をViewに追加
     @discord.ui.select(
-        cls=UserSelect,
-        placeholder="メンバー選択",
-        min_values=2,
-        max_values=10
+        cls=UserSelect, placeholder="メンバー選択", min_values=2, max_values=10
     )
-    async def select_callback(self, interaction: discord.Interaction, select: UserSelect):
+    async def select_callback(
+        self, interaction: discord.Interaction, select: UserSelect
+    ):
         # 選択完了時の処理
-        team_view = TeamControlView(self.start_time, self.count, self.record, select.values)
+        team_view = TeamControlView(
+            self.start_time, self.count, self.record, select.values
+        )
         await interaction.response.edit_message(
-            embed=team_view.current_embed,
-            view=team_view
+            embed=team_view.current_embed, view=team_view
         )
--- SplatoonAssistant/WeaponRandomSelectView.py
+++ SplatoonAssistant/WeaponRandomSelectView.py
@@ -5,9 +5,8 @@ from discord.ui import View, Button
 
 class WeaponRandomSelectView(View):
 
-
     def __init__(self, weapons, end=None):
-        super().__init__(timeout=300)
+        super().__init__(timeout=None)
         self.weapons = weapons
         self.end = end
 
@@ -21,86 +20,73 @@ class WeaponRandomSelectView(View):
             self.category = end
         # カテゴリーごとの表示形式設定
         self.category_dict = {
-            "weapon" : {
-                "name" : "ブキ",
-                "emoji" : "⚔️"
-            },
-            "main" : {
-                "name" : "メイン",
-                "emoji" : "🔫"
-            },
-            "sub" : {
-                "name" : "サブ",
-                "emoji" : "💣"
-            },
-            "special" : {
-                "name" : "スペシャル",
-                "emoji" : "🚀"
-            }
+            "weapon": {"name": "ブキ", "emoji": "⚔️"},
+            "main": {"name": "メイン", "emoji": "🔫"},
+            "sub": {"name": "サブ", "emoji": "💣"},
+            "special": {"name": "スペシャル", "emoji": "🚀"},
         }
         self.name = self.category_dict[self.category]["name"]
         self.emoji = self.category_dict[self.category]["emoji"]
 
         # 画像パス生成用の辞書
         self.gamewith_image_dict = {
-            "main" : {
-                "alias" : "type",
-                "shooter" : 1,
-                "charger" : 2,
-                "blaster" : 3,
-                "roller" : 4,
-                "brush" : 5,
-                "slosher" : 6,
-                "spinner" : 7,
-                "maneuver" : 8,
-                "brella" : 9,
-                "wiper" : 10,
-                "stringer" : 11,
+            "main": {
+                "alias": "type",
+                "shooter": 1,
+                "charger": 2,
+                "blaster": 3,
+                "roller": 4,
+                "brush": 5,
+                "slosher": 6,
+                "spinner": 7,
+                "maneuver": 8,
+                "brella": 9,
+                "wiper": 10,
+                "stringer": 11,
             },
-            "sub" : {
-                "alias" : "sub",
-                "splashshield" : 1,
-                "curlingbomb" : 2,
-                "quickbomb" : 3,
-                "poisonmist" : 4,
-                "pointsensor" : 5,
-                "jumpbeacon" : 6,
-                "splashbomb" : 7,
-                "trap" : 8,
-                "robotbomb" : 9,
-                "sprinkler" : 10,
-                "tansanbomb" : 11,
-                "torpedo" : 12,
-                "kyubanbomb" : 13,
-                "linemarker" : 14,
+            "sub": {
+                "alias": "sub",
+                "splashshield": 1,
+                "curlingbomb": 2,
+                "quickbomb": 3,
+                "poisonmist": 4,
+                "pointsensor": 5,
+                "jumpbeacon": 6,
+                "splashbomb": 7,
+                "trap": 8,
+                "robotbomb": 9,
+                "sprinkler": 10,
+                "tansanbomb": 11,
+                "torpedo": 12,
+                "kyubanbomb": 13,
+                "linemarker": 14,
             },
-            "special" : {
-                "alias" : "sp",
-                "missile" : 1,
-                "amefurashi" : 2,
-                "kanitank" : 3,
-                "nicedama" : 4,
-                "jetpack" : 5,
-                "megaphone51" : 6,
-                "greatbarrier" : 7,
-                "ultrahanko" : 8,
-                "kyuinki" : 9,
-                "energystand" : 10,
-                "hopsona" : 11,
-                "sameride" : 12,
-                "shokuwander" : 13,
-                "tripletornado" : 14,
-                "ultrashot" : 15,
-                "teioika" : 16,
-                "decoy" : 17,
-                "suminagasheet" : 18,
-                "ultra_chakuchi" : 19,
-            }
-        }          
+            "special": {
+                "alias": "sp",
+                "missile": 1,
+                "amefurashi": 2,
+                "kanitank": 3,
+                "nicedama": 4,
+                "jetpack": 5,
+                "megaphone51": 6,
+                "greatbarrier": 7,
+                "ultrahanko": 8,
+                "kyuinki": 9,
+                "energystand": 10,
+                "hopsona": 11,
+                "sameride": 12,
+                "shokuwander": 13,
+                "tripletornado": 14,
+                "ultrashot": 15,
+                "teioika": 16,
+                "decoy": 17,
+                "suminagasheet": 18,
+                "ultra_chakuchi": 19,
+            },
+        }
 
         self.current_embed = None
         self.random_selection()
-
 
     # ブキに関する一覧を取得する関数
     def get_all(self):
@@ -116,7 +102,6 @@ class WeaponRandomSelectView(View):
         # print(get_dict)
         return get_dict
 
-
     # ブキ関連の画像URLを取得する関数
     def set_image_url(self, selected_key):
         image_url = ""
@@ -130,7 +115,6 @@ class WeaponRandomSelectView(View):
             image_url = f"https://img.gamewith.jp/article_tools/splatoon3/gacha/{category}{num}.png"
         return image_url
 
-
     def random_selection(self):
         # ランダムに選択
         select_list = list(self.get_all().items())
@@ -138,28 +122,26 @@ class WeaponRandomSelectView(View):
         self.selected_key = selected[0]
         self.selected_value = selected[1]
         image_url = self.set_image_url(self.selected_key)
-        
+
         # Embedの作成
         embed = discord.Embed(
             title=f"{self.emoji} ランダム {self.name}選択 (確認中)",
-            color=discord.Color.orange()
+            color=discord.Color.orange(),
         )
-        embed.add_field(name=f"選択された{self.name}", value=self.selected_value, inline=True)
+        embed.add_field(
+            name=f"選択された{self.name}", value=self.selected_value, inline=True
+        )
         embed.set_thumbnail(url=image_url)
         # embedセット
         self.current_embed = embed
 
-
     # 「再選択」ボタンの定義
     @discord.ui.button(label="再選択", style=discord.ButtonStyle.secondary, emoji="🔁")
     async def reselect_button(self, interaction: discord.Interaction, button: Button):
-        await interaction.response.defer() # 処理中であることを表示
+        await interaction.response.defer()  # 処理中であることを表示
         self.random_selection()
-        # メッセージの編集 
-        await interaction.edit_original_response(
-            embed=self.current_embed
-        )
-
+        # メッセージの編集
+        await interaction.edit_original_response(embed=self.current_embed)
 
     # 「確定」ボタンの定義
     @discord.ui.button(label="確定", style=discord.ButtonStyle.success, emoji="✅")
@@ -167,7 +149,9 @@ class WeaponRandomSelectView(View):
         await interaction.response.defer()  # 処理中であることを表示
         self.current_embed.title = "✅ 決定！"
         self.current_embed.color = discord.Color.green()
-        self.current_embed.set_footer(text=f"選択が確定しました。確定者: {interaction.user.display_name}")
+        self.current_embed.set_footer(
+            text=f"選択が確定しました。確定者: {interaction.user.display_name}"
+        )
 
         # View全体を無効化
         self.stop()
@@ -175,7 +159,4 @@ class WeaponRandomSelectView(View):
             child.disabled = True
 
         # メッセージを更新し、ボタンを無効化
-        await interaction.edit_original_response(
-            embed=self.current_embed,
-            view=self
-        )
+        await interaction.edit_original_response(embed=self.current_embed, view=self)
